e0948ff2e6b3c0c0c66fe573dcfd23dfdbe77fa5
Leandro Moscheni <leandromoscheni@gmail.com>
2021-12-03 22:56:11 -0300

    Change styles for mobile devices

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,14 +5,15 @@ type HomePageProps = {};
 
 export default function HomePage(_props: HomePageProps): JSX.Element {
   return (
-    <div className="p-5">
-      <main className="p-5 text-center border-1 rounded-md">
-        <h1 className="text-4xl font-bold mb-5">F-Chat[Firebase Chat]</h1>
-        <p className="text-xl">
-          You only need login with google, and enjoy the experience!
-        </p>
+    <div className="p-2 sm:p-5">
+      <main className="px-2 py-3 bg-gray-200 text-center border-2 rounded-md sm:p-5">
+        <h1 className="text-2xl mb-2 font-bold sm:text-4xl sm:mb-5">
+          Firebase Chat
+        </h1>
+        <p className="text-xl mb-2 sm:text-xl">Your free chatroom.</p>
+        <p>You only need login with google, and enjoy the experience!</p>
         <button
-          className="m-2 px-4 py-2 rounded-md text-white bg-green-600"
+          className="m-2 px-4 py-2 rounded-md text-white bg-green-600 hover:text-black hover:bg-gray-300 hover:font-bold"
           onClick={() => signInWithPopup(authenticator, authenticationProvider)}
         >
           Login
